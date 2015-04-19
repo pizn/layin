401cba3f7e01ffe3226b644e80328ae10056d494
pizn <pizner@gmail.com>
2015-04-20 00:10:14 +0800

    好久不见

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ### Layin
 
-Coming soom...
+
---
+++ gulpfile.js
@@ -0,0 +1,5 @@
+var gulp = require('gulp'),
+    customMedia = require('rework-custom-media'),
+    connect = require('gulp-connect'),
+    $ = require('gulp-load-plugins')();
+
